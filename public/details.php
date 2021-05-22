6b2632911b305581bd1d210095ed3f00bf0cc3c5
Bobbykelso <barth.aurelien182@gmail.com>
2021-05-22 23:10:07 +0200

    Quete 3

--- public/details.php
+++ public/details.php
@@ -6,4 +6,4 @@ require_once __DIR__ . '/../config/twig.php';
 
 $email = 'barth.aurelient182@gmail.com';
 
-echo $twig->render('home.html.twig', ['email' => $email]);
+echo $twig->render('details.html.twig', ['email' => $email]);
